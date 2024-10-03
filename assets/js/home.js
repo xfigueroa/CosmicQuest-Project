a04071963562006fc3ab77ca0220d750f66a5341
Xavier Figueroa <xfigueroa28@gmail.com>
2024-10-03 16:14:46 -0400

    update modal and added img to this week section

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,3 +1,20 @@
+
+const faqsOpen = document.getElementById('faqs-open');
+const modalfaqsContaier = document.getElementById('modal-faqs-contaier');
+const faqsClose = document.getElementById('faqs-close');
+
+faqsOpen.addEventListener('click',() =>{
+    modalfaqsContaier.classList.add('active')
+});
+faqsClose.addEventListener('click',() =>{
+    modalfaqsContaier.classList.remove('active')
+});
+
+
+
+
+
+
 function toggleAnswer(index) {
     const answers = document.querySelectorAll('.answer');
     answers.forEach((answer, i) => {
